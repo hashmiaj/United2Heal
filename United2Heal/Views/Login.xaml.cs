5a2b16acd06d0b623064d6b5033b83a00af82f33
Gurwinder Singh <Singhg22@vcu.edu>
2020-03-18 12:19:33 -0400

    Created the Login Page
    * Made LoginViewModel
    * Made Login XAML and XAML.CS

---
+++ United2Heal/Views/Login.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using United2Heal.ViewModels;
+using Xamarin.Forms;
+
+namespace United2Heal.Views
+{
+    public partial class Login : ContentPage
+    {
+        LoginViewModel ViewModel;
+        public Login(String school)
+        {
+            InitializeComponent();
+            ViewModel = new LoginViewModel();
+            
+        }
+    }
+}
