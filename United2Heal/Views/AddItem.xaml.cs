483fd1f1525af4f75e545818d8714c346953eb10
Gurwinder Singh <Singhg22@vcu.edu>
2020-04-26 22:00:23 -0400

    Created Add Item Page
    *Linked Main menu and Add Item Page.
    *Added UI for Add Item Page.

---
+++ United2Heal/Views/AddItem.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace United2Heal.Views
+{
+    public partial class AddItem : ContentPage
+    {
+        public AddItem()
+        {
+            InitializeComponent();
+        }
+
+        void Submit_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
