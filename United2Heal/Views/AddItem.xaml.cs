170bfe73d06dd08903c6c72fcd9e1dd21630776b
Abdullah Hashmi <abdullah.hashmi21@gmail.com>
2020-06-12 00:41:25 -0400

    Adding alerts to add item page

--- United2Heal/Views/AddItem.xaml.cs
+++ United2Heal/Views/AddItem.xaml.cs
@@ -12,9 +12,26 @@ namespace United2Heal.Views
             InitializeComponent();
         }
 
-        void Submit_Clicked(System.Object sender, System.EventArgs e)
+        public async void Submit_Clicked(object sender, EventArgs e)
         {
+            if(string.IsNullOrEmpty(ItemEntry.Text))
+            {
+                await DisplayAlert("Blank Name!", "Please enter a name for the item.", "Okay");
+            }
+            else
+            {
+                bool answer = await DisplayAlert("Confirm", "Are you sure?", "Submit", "Cancel");
 
+                if(answer)
+                {
+                    //Post request code...
+
+                    await DisplayAlert("Submitted!", "Item: " + ItemEntry.Text + " has been added", "Okay");
+                    await Navigation.PopModalAsync();
+
+                    //Code to navigate to the item page
+                }
+            }
         }
     }
 }
