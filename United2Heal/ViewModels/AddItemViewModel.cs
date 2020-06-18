7f9c9a11fbd1f4afc2dbd28af5cb38094a789042
Abdullah Hashmi <abdullah.hashmi21@gmail.com>
2020-06-17 21:55:24 -0400

    Finishing add item view model

--- United2Heal/ViewModels/AddItemViewModel.cs
+++ United2Heal/ViewModels/AddItemViewModel.cs
@@ -22,9 +22,7 @@ namespace United2Heal.ViewModels
         {
             Item item = new Item();
             item.name = itemName;
-            String code = "";
            
-
             HttpClient client = new HttpClient();
 
             var authData = GlobalVariables.SchoolName.Equals("VCU") ? string.Format("{0}:{1}", "VCU", "united") : string.Format("{0}:{1}", "GMU", "u2hgmu");
@@ -33,36 +31,11 @@ namespace United2Heal.ViewModels
 
             Uri uri = new Uri(String.Format("https://u2h.herokuapp.com/api/v1.0/items"));
 
-            var response = await client.GetAsync(uri);
-
-            if (response.IsSuccessStatusCode)
-            {
-                String content = await response.Content.ReadAsStringAsync();
-                Items = new List<Item>();
-
-                ItemResults itemResults = JsonConvert.DeserializeObject<ItemResults>(content);
-                var sortedList = itemResults.Items.OrderBy(x => x.id);
-                int maxInt = Int32.MinValue;
-
-                foreach(var obj in sortedList)
-                {
-                    int num = Int32.Parse(obj.id);
-                    if (num > maxInt)
-                        maxInt = num;
-                }
-
-                code = maxInt.ToString();
-            }
-
-            int itemCode = Int32.Parse(code) + 1;
-            String url = "https://u2h.herokuapp.com/api/v1.0/items";
-            item.id = itemCode.ToString();
-
             var postItem = JsonConvert.SerializeObject(item);
 
             StringContent postContent = new StringContent(postItem, Encoding.UTF8, "application/json");
             
-            var postResponse = await client.PostAsync(uri, postContent);
+            await client.PostAsync(uri, postContent);
         }
     }
 }
